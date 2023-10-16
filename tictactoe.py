be97ba898e3a4472c20cbd11f3f9cd4bfa52f798
Dale Doster <dale.doster1@marist.edu>
2023-10-16 15:43:57 -0400

    Added random AI moves as well as improved user input handling

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,5 @@
+from random import randint
+
 __project__ = "TicTacToe"
 __author__ = "Dale Doster"
 __version__ = "0.1.0"
@@ -7,7 +9,7 @@ __license__ = "MIT"
 board = [" " for _ in range(9)]
 
 
-def create_board(game_board):
+def print_board(game_board):
     print(f" {game_board[0]} | {game_board[1]} | {game_board[2]}")
     print("---+---+---")
     print(f" {game_board[3]} | {game_board[4]} | {game_board[5]}")
@@ -15,24 +17,53 @@ def create_board(game_board):
     print(f" {game_board[6]} | {game_board[7]} | {game_board[8]}\n")
 
 
-def update_board():
-    print()
-
-
 def init_game():
-    create_board(board)
+    print_board(board)
 
 
-def request_input():
-    user_input = input("Select cell 1-9: ");
-    print(user_input)
+def run_game():
+    while True:
+        print_board(get_user_move())
+        print_board(get_AI_move())
+        check_for_win()
+
+
+def is_valid_position(position):
+    return True if (position >= 0 and position <= 8) and (board[position] == " ") else False
+
+
+def get_user_move():
+    user_input = int(input("Player X - Select cell 1-9: "))
+    
+    while not is_valid_position(user_input - 1):
+        user_input = int(input("Oops, that is not a valid position - Select cell 1-9: "))
+
+    print("")
+    board[user_input - 1] = "X"
+    return board
+
+
+def get_AI_move():
+    random_value = randint(0, 8)
+    print(random_value)
+    print(random_value)
+    print(random_value)
+    print(random_value)
+    print(random_value)
+    print(random_value)
+
+    while not is_valid_position(random_value):
+        random_value = randint(0, 8)
+
+    board[random_value] = "O"
+    return board
 
 
 def main():
     print(f"{__project__}\nBy {__author__}\nv{__version__}\n")
 
     init_game()
-    request_input()
+    run_game()
 
 
 if __name__ == "__main__":
