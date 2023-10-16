da0458188f214fa01ae450260afedcc670e21ce9
Dale Doster <dale.doster1@marist.edu>
2023-10-16 12:22:07 -0400

    Initialized TicTacToe Python project

---
+++ tictactoe.py
@@ -0,0 +1,12 @@
+__project__ = "TicTacToe"
+__author__ = "Dale Doster"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def main():
+    print(f"{__project__}\nBy {__author__}\nv{__version__}")
+
+
+if __name__ == "__main__":
+    main()
