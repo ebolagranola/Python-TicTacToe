6220dd1c6651185f6e87c24aa0bb169bb02517df
Dale Doster <dale.doster1@marist.edu>
2023-10-16 12:31:33 -0400

    Init implementation of TicTacToe board

--- tictactoe.py
+++ tictactoe.py
@@ -4,8 +4,25 @@ __version__ = "0.1.0"
 __license__ = "MIT"
 
 
+board = [" " for _ in range(9)]
+
+
+def create_board():
+    print(f" {board[0]} | {board[1]} | {board[2]} ")
+    print("---+---+---")
+    print(f" {board[3]} | {board[4]} | {board[5]} ")
+    print("---+---+---")
+    print(f" {board[6]} | {board[7]} | {board[8]} ")
+
+
+def init_game():
+    create_board()
+
+
 def main():
     print(f"{__project__}\nBy {__author__}\nv{__version__}")
+
+    init_game()
 
 
 if __name__ == "__main__":
